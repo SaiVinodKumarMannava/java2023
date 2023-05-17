44c4daea7c2b7bf61111c857084e35c5c1e6d922
Vinod <mannavasai999@gmail.com>
2023-05-17 13:19:43 -0500

    programs by vinod

---
+++ src/chase/com/InsideTheForLoop.java
@@ -0,0 +1,10 @@
+package chase.com;
+
+public class InsideTheForLoop {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+           // System.out.println("1-10 number will print in a row");
+        }
+    }
+}
---
+++ src/chase/com/OutsideTheForLoop.java
@@ -0,0 +1,11 @@
+package chase.com;
+
+public class OutsideTheForLoop {
+    public static void main(String[] args){
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i);
+        }
+        System.out.println(i+ " Out side the for loop");
+    }
+}
